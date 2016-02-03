f4ca1f63536977f829a0e3c76d29b02d4a1ad436
Chad Wescott <cwescott@coopervision.com>
2016-02-02 21:02:35 -0500

    Added Inventory Audit page and lumber audit form.

---
+++ Thermory.Web/Controllers/InventoryAuditController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Thermory.Business;
+using Thermory.Web.Models;
+
+namespace Thermory.Web.Controllers
+{
+    public class InventoryAuditController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new InventoryWorksheet
+            {
+                LumberProductCategories = CommandDirectory.Instance.GetAllLumberProductsWithInventory()
+            };
+            return View(model);
+        }
+
+    }
+}
