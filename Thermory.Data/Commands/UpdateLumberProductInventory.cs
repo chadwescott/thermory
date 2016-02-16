9ececb91a72148d09e17a5b166f8de2bd6136362
Chad Wescott <cwescott@coopervision.com>
2016-02-16 16:09:55 -0500

    Adding missing file.

---
+++ Thermory.Data/Commands/UpdateLumberProductInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Thermory.Domain;
+
+namespace Thermory.Data.Commands
+{
+    internal class UpdateLumberProductInventory : DatabaseCommand
+    {
+        private readonly ILumberProduct _lumberProduct;
+
+        public UpdateLumberProductInventory(ILumberProduct lumberProduct)
+        {
+            _lumberProduct = lumberProduct;
+        }
+
+        protected override void OnExecute(ThermoryContext context)
+        {
+            var dbLumberProduct = context.LumberProducts.SingleOrDefault(lp => lp.Id == _lumberProduct.Id);
+            if (dbLumberProduct == null)
+                return;
+
+            dbLumberProduct.Quantity = _lumberProduct.Quantity;
+            context.SaveChanges();
+        }
+    }
+}
