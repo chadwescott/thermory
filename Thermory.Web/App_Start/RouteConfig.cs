4b6c88551ff4ebc66663279dc9e205ee0677622d
Chad Wescott <cwescott@coopervision.com>
2016-02-20 16:05:46 -0500

    A few styling tweaks and changed the default page to the home page.

--- Thermory.Web/App_Start/RouteConfig.cs
+++ Thermory.Web/App_Start/RouteConfig.cs
@@ -9,7 +9,7 @@ namespace Thermory.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "InventoryWorksheet", action = "Index", id = UrlParameter.Optional }
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
