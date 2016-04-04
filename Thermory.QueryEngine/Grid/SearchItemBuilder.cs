0fcff8971450035533bf564d07ed32cdbf1e43ad
Chad Wescott <cwescott@coopervision.com>
2016-04-03 20:47:43 -0400

    Fixed date filtering.

--- Thermory.QueryEngine/Grid/SearchItemBuilder.cs
+++ Thermory.QueryEngine/Grid/SearchItemBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Reflection;
@@ -30,14 +31,35 @@ namespace Thermory.QueryEngine.Grid
                     var sqlBuilder = new StringBuilder();
                     var item = new SearchItem { Parameters = new List<IDataParameter>() };
 
-                    for (int i = 0; i < filter.value.Count; ++i)
+                    if (filter.type == "date")
                     {
-                        var parameter = GetDbParameterFactory().MakeDbParameter(column.Name, i, filter.value[i]);
-                        item.Parameters.Add(parameter);
+                        var startDate = DateTime.Parse(filter.value[0].ToString());
+                        var endDate =
+                            (filter.@operator == "between" ? DateTime.Parse(filter.value[1].ToString()) : startDate)
+                                .AddDays(1).AddMilliseconds(-1);
+                        var startDateParamString = string.Format("{0}StartDate", column.Name);
+                        var endDateParamString = string.Format("{0}EndDate", column.Name);
+                        var startDateParam = GetDbParameterFactory().MakeDbParameter(startDateParamString, 0, startDate);
+                        var endDateParam = GetDbParameterFactory().MakeDbParameter(endDateParamString, 0, endDate);
+                        item.Parameters.Add(startDateParam);
+                        item.Parameters.Add(endDateParam);
+                        sqlBuilder.AppendFormat("{0} BETWEEN {1} AND {2}", column.Name, startDateParam.ParameterName,
+                            endDateParam.ParameterName);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < filter.value.Count; ++i)
+                        {
+                            var parameter = GetDbParameterFactory().MakeDbParameter(column.Name, i, filter.value[i]);
+                            item.Parameters.Add(parameter);
 
-                        if (i > 0) { sqlBuilder.Append(" OR "); }
+                            if (i > 0)
+                            {
+                                sqlBuilder.Append(" OR ");
+                            }
 
-                        sqlBuilder.AppendFormat(BuildCondition(column.Name, parameter, filter.@operator));
+                            sqlBuilder.AppendFormat(BuildCondition(column.Name, parameter, filter.@operator));
+                        }
                     }
 
                     item.DetailSql = sqlBuilder.ToString();
