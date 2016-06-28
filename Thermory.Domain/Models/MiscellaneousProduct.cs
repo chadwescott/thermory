3e35b969ae19d6436a6a9a04a5de8dc5bf47656e
Chad Wescott <cwescott@coopervision.com>
2016-06-27 21:48:57 -0400

    Added weight to miscellaneous products and added weight and description to miscellaneous product catalog.

--- Thermory.Domain/Models/MiscellaneousProduct.cs
+++ Thermory.Domain/Models/MiscellaneousProduct.cs
@@ -19,6 +19,8 @@ namespace Thermory.Domain.Models
 
         public string Description { get; set; }
 
+        public decimal? Weight { get; set; }
+
         public int SortOrder { get; set; }
 
         public int Quantity { get; set; }
