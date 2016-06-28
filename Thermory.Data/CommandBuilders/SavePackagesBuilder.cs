0c268887f06e209620209a4442b7f12ef477b80d
Chad Wescott <cwescott@coopervision.com>
2016-06-28 17:01:08 -0400

    Added validation to ensure that all products on an order are packaged.

--- Thermory.Data/CommandBuilders/SavePackagesBuilder.cs
+++ Thermory.Data/CommandBuilders/SavePackagesBuilder.cs
@@ -12,9 +12,29 @@ namespace Thermory.Data.CommandBuilders
             miscLineItems = miscLineItems ?? new PackageMiscellaneousLineItem[0];
 
             var order = DatabaseCommandDirectory.Instance.GetOrderById(orderId);
+            ValidateAllProductsPackaged(order, lumberLineItems, miscLineItems);
             Commands = !order.Packages.Any()
                 ? new CreatePackagesBuilder(userId, order, lumberLineItems, miscLineItems).Commands
                 : new EditPackagesBuilder(userId, order, lumberLineItems, miscLineItems).Commands;
+        }
+
+        private void ValidateAllProductsPackaged(Order order, PackageLumberLineItem[] lumberLineItems, PackageMiscellaneousLineItem[] miscLineItems)
+        {
+            if (!AllProductsPackage(order, lumberLineItems, miscLineItems))
+                throw new Exception("Not all products on the order have been packaged.");
+        }
+
+        private bool AllProductsPackage(Order order, PackageLumberLineItem[] lumberLineItems, PackageMiscellaneousLineItem[] miscLineItems)
+        {
+            return
+                order.OrderLumberLineItems.All(
+                    lumberLineItem =>
+                        lumberLineItems.Where(li => li.LumberProductId == lumberLineItem.LumberProductId)
+                            .Sum(li => li.Quantity) == lumberLineItem.Quantity) &&
+                order.OrderMiscellaneousLineItems.All(
+                    miscLineItem =>
+                        miscLineItems.Where(li => li.MiscellaneousProductId == miscLineItem.MiscellaneousProductId)
+                            .Sum(li => li.Quantity) == miscLineItem.Quantity);
         }
     }
 }
